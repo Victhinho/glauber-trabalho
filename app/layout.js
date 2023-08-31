311458f25b951cd9ecb290a1147d5aad9340fdd7
Victhinho <jvla2@aluno.ifal.edu.br>
2023-08-31 16:33:58 -0300

    Mudanças

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,27 @@
 import './globals.css'
-
+import styles from './styles.module.css'
+// import { Quicksand } from 'next/font/google'
 export const metadata = {
   title: 'Travel Landing Page',
+}
+
+
+
+function Logo(){
+  return(
+    <p className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
+  )
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>
+          <Logo>Ready&Go</Logo>
+        </header>
+        
+        {children}</body>
     </html>
   )
 }
