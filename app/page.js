a061be5a44540b057fcefa7bbf76f75f73f4ff2e
Victhinho <jvla2@aluno.ifal.edu.br>
2023-10-21 11:33:53 -0300

    estrutura- adição de redes sociais. Precisa de atualização

--- app/page.js
+++ app/page.js
@@ -5,6 +5,9 @@ import Hawaii from '../public/hawaii.png'
 import Cyprus from '../public/cyprus.png'
 import Bali from '../public/bali.png'
 import Seta from '../public/arrow-right.svg'
+import Facebook from '../public/facebook.svg'
+import Twitter from '../public/twitter.svg'
+import Linkedin from '../public/linkedin.svg'
 
 function Botao({ x, y }) {
   return (
@@ -32,6 +35,20 @@ function Cards({children, x, y}) {
   )
 }
 
+function Social(imagem){
+  return(
+    <Image src={imagem} className={styles.imagemRedesSociais}/>
+  )
+}
+
+function Sociais({children, x, y}) {
+  return(
+    <div style={{left: x, top: y}} className={styles.social}>
+      {children}
+    </div>
+  )
+}
+
 export default function Home() {
   return (
     <>
@@ -41,6 +58,11 @@ export default function Home() {
         <Card localidade="Cyprus" imagem={Cyprus}/>
         <Card localidade="Bali" imagem={Bali}/>
       </Cards>
+      <Sociais x={1175} y={843}>
+        <Social imagem={Facebook}/>
+        <Social imagem={Twitter}/>
+        <Social imagem={Linkedin}/>
+      </Sociais>
     </>
   )
 }
